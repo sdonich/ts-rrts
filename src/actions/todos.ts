b645309792274d74835c71b1891001eea1061179
sdonich <sdonich@gmail.com>
2019-10-25 20:15:45 +0200

    some fix

--- src/actions/todos.ts
+++ src/actions/todos.ts
@@ -21,10 +21,10 @@ export interface DeleteTodoAction {
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
 export const fetchTodos = () => {
-  return async (dispatch: Dispatch<FetchTodosAction>) => {
+  return async (dispatch: Dispatch) => {
     const response = await axios.get<Todo[]>(url);
 
-    dispatch({
+    dispatch<FetchTodosAction>({
       type: ActionTypes.fetchTodos,
       payload: response.data
     });
